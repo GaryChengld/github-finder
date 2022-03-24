a2098a82daafa040a9b17dbcabccbe225cad6496
GaryChengld <chengld@yahoo.com>
2022-03-23 21:21:55 -0400

    search users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,19 +14,22 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  // Get initial users (testing purpose)
-  const fetchUsers = async () => {
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading()
-    const response = await fetch(`${GITHUB_URL}/users`, {
+    const params = new URLSearchParams({
+      q: text,
+    })
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
 
-    const data = await response.json()
+    const { items } = await response.json()
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     })
   }
 
@@ -35,9 +38,18 @@ export const GithubProvider = ({ children }) => {
     dispatch({ type: 'SET_LOADING' })
   }
 
+  const clearUsers = () => {
+    dispatch({ type: 'CLEAR_USERS' })
+  }
+
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
