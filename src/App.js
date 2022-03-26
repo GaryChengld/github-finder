d4957814332f896318c2231d0db160d9cc9f09d9
GaryChengld <chengld@yahoo.com>
2022-03-25 21:30:27 -0400

    user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import User from './pages/User'
+
 import { GithubProvider } from './context/github/GithubContext'
 import { AlertProvider } from './context/alert/AlertContext'
 
@@ -15,9 +18,11 @@ function App() {
           <div className='flex flex-col justify-between h-screen'>
             <Navbar />
             <main className='container mx-auto px-3 pb-12'>
+              <Alert />
               <Routes>
                 <Route path='/' element={<Home />}></Route>
                 <Route path='/about' element={<About />}></Route>
+                <Route path='/user/:login' element={<User />} />
                 <Route path='/notfound' element={<NotFound />}></Route>
                 <Route path='/*' element={<NotFound />}></Route>
               </Routes>
